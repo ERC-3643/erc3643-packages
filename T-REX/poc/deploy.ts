39b232a67854b9eccd604e4cb8e1ef6b3f523e10
Valeriy Ilihmetov <11663800+ValeriyDP@users.noreply.github.com>
2023-09-01 17:20:20 +0300

    INITIAL COMMIT

---
+++ T-REX/poc/deploy.ts
@@ -0,0 +1,14 @@
+import { deploySuiteWithModularCompliancesFixture, deploySuiteWithModularCompliancesModulesFixture, deploySuiteWithModuleComplianceBoundToWallet } from '../test/fixtures/deploy-full-suite.fixture'
+// import { inspect } from 'util';
+
+(async () => {
+
+  // const result = await deploySuiteWithModuleComplianceBoundToWallet();
+  // const result = await deploySuiteWithModularCompliancesFixture();
+  const result = await deploySuiteWithModularCompliancesModulesFixture();
+
+  for(const [key, contract] of Object.entries(result.suite)) {
+    console.log(key.padEnd(25, ' '), contract.address);
+  }
+  // console.log('TokenAddress', result.suite.token.address);
+})()
