239f78fd8ce96d539d13a92df0192f5adc82dd50
Oleksii Sirochenko <oleksii.sirochenko@dev.pro>
2023-09-14 12:37:01 +0300

    Header update

--- project-root/apps/demo-react/src/Header/Header.tsx
+++ project-root/apps/demo-react/src/Header/Header.tsx
@@ -4,16 +4,12 @@ import { useEthers, useEtherBalance } from '@usedapp/core'
 
 const Header = () => {
   const { account } = useEthers()
-  const userBalance = useEtherBalance(account)
-  const coinsAmount = userBalance?.toString()
-
   return (
     <StyledHeader>
       <StyledRow>
         <StyledLeftCol>ERC3643</StyledLeftCol>
         <StyledRightCol>
           {account && <StyledPill>{account}</StyledPill>}
-          {coinsAmount && <StyledPill>{coinsAmount} ETH</StyledPill>}
           <ConnectButton />
         </StyledRightCol>
       </StyledRow>
